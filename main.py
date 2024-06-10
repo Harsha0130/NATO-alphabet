0042553e68f12b83273db7a89ce243162fa260bf
Harsha <harshahm0130@gmail.com>
2024-06-10 21:04:12 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+print(data.to_dict())
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
