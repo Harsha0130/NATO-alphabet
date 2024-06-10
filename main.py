b6765da22372338c35ee9f9f5124481d485f5597
Harsha <harshahm0130@gmail.com>
2024-06-10 21:31:40 +0530

    Final Commit

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-print(data.to_dict())
-phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(phonetic_dict)
+# print(data.to_dict())
 
+phonetic_data = {row.letter: row.code for [index, row] in data.iterrows()}
+# print(phonetic_data)
+
+word = input("Enter thr word : ").upper()
+output_list = [phonetic_data[letter] for letter in word]
+print(output_list)
